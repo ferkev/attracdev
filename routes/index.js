afa8e5cd73dd34329019e3a7b7a372d35c381146
Kevin Le Franc <ferkev@live.fr>
2018-05-14 15:49:50 +0200

    model

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,35 @@ router.get('/', function(req, res, next) {
 
 mongoose.connect("mongodb://ferkev:ferkev@ds119150.mlab.com:19150/attractdev",
     function(err) {
-     //console.log(err);
+     console.log(err);
     }
 );
 
+var founderSchema = mongoose.Schema({
+  entrepriseName: String,
+  email: String,
+  password: String
+});
+
+var developerSchema = mongoose.Schema({
+  email: String,
+  password: String,
+  project_id: String
+})
 
 
+var projectSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  creationDate: { type: Date, default: Date.now },
+  effective: Number,
+  entrepriseName: String,
+  founder_id: String
+})
 
+var developerModel = mongoose.model('developers', developerSchema);
+var  founderModel = mongoose.model('founders', founderSchema);
+var projectSchema = mongoose.model('projects', projectSchema);
 
 
 module.exports = router;
