135424a405eb299af7b3b0523f05cf6da4d3bcf3
Kevin Le Franc <ferkev@live.fr>
2018-05-14 13:35:31 +0200

    session et mongoose

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var session = require('express-session');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +23,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(
+ session({
+  secret: 'a4f8071f-c873-4447-8ee2',
+  resave: false,
+  saveUninitialized: false,
+ })
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
