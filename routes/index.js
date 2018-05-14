649f1251e741744d43f2eddf8bb30c93a691ce34
Kevin Le Franc <ferkev@live.fr>
2018-05-14 13:53:35 +0200

    creation datavase

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+var mongoose= require('mongoose');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+mongoose.connect("mongodb://ferkev:ferkev@ds119150.mlab.com:19150/attractdev",
+    function(err) {
+     //console.log(err);
+    }
+);
+
+
+
+
+
 
 module.exports = router;
