3a7865a52f814bd5ecfa2240d56e215a6ce5bb69
Gilles Chris <progchris@live.fr>
2018-05-15 13:40:36 +0200

    gestion popup formulaire login

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,17 +4,35 @@ const search = document.getElementById('search');
 
 btn.addEventListener('click',
 	function(e) {
-		e.preventDefault()
+		e.preventDefault();
 		search.classList.remove('hide');
 		search.classList.add('show');
 	}
 );
 
-document.getElementById('login').addEventListener('click',
+document.getElementById('fun_login').addEventListener('click',
+	function(e) {
+		e.preventDefault();
+		document.getElementById('tab_container_fun').classList.remove('hide');
+		document.getElementById('tab_container_fun').classList.add('show');
+
+		if(document.getElementById('tab_container_dev').className ==='tab_container show'){
+			document.getElementById('tab_container_dev').classList.remove('show');
+			document.getElementById('tab_container_dev').classList.add('hide');
+		}
+	}
+);
+
+document.getElementById('dev_login').addEventListener('click',
 	function(e) {
 		e.preventDefault()
-		document.getElementById('tab_container').classList.remove('hide');
-		document.getElementById('tab_container').classList.add('show');
+		document.getElementById('tab_container_dev').classList.remove('hide');
+		document.getElementById('tab_container_dev').classList.add('show');
+
+		if(document.getElementById('tab_container_fun').className ==='tab_container show'){
+			document.getElementById('tab_container_fun').classList.remove('show');
+			document.getElementById('tab_container_fun').classList.add('hide');
+		}
 	}
 );
 
@@ -22,9 +40,10 @@ for(var i = 0; i<document.getElementsByClassName('close').length; i++){
 	document.getElementsByClassName('close')[i].addEventListener("click",
 	  function(e) {
 	    e.preventDefault();
-	    document.getElementById('tab_container').classList.remove('show');
-	    document.getElementById('tab_container').classList.add('hide');
-			document.getElementById('tab_container').style.opacity = "1";
+	    document.getElementById('tab_container_fun').classList.remove('show');
+	    document.getElementById('tab_container_fun').classList.add('hide');
+			document.getElementById('tab_container_dev').classList.remove('show');
+	    document.getElementById('tab_container_dev').classList.add('hide');
 	  }
 	);
 }
